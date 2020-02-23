674c53d8da4e352f1c17aab35a64047c6b53bc96
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-23 14:10:23 +0500

    Add InputService

---
+++ KnapsackProblem.Blazor/Data/InputService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Blazor.FileReader;
+using KnapsackProblem.BlazorApp.Data.Knapsack;
+using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace KnapsackProblem.BlazorApp.Data
+{
+    public class InputService
+    {
+        private IFileReference _file;
+
+        public async Task<KnapsackInput> Input()
+        {
+            await using var stream = await _file.CreateMemoryStreamAsync(4096);
+            using var reader = new StreamReader(stream);
+            var content = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<KnapsackInput>(content);
+        }
+
+        public void SetFromFile(IFileReference file)
+        {
+            if (_file is IDisposable obj)
+            {
+                obj.Dispose();
+            }
+            _file = file;
+        }
+    }
+}
--- KnapsackProblem.Blazor/Startup.cs
+++ KnapsackProblem.Blazor/Startup.cs
@@ -24,7 +24,9 @@ namespace KnapsackProblem.BlazorApp
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddFileReaderService();
+
             services.AddSingleton<ImplementationRegistryService>();
+            services.AddSingleton<InputService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

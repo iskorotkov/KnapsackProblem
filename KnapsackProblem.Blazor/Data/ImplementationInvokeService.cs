fbec7bd32e4a95d8c3b4864e4e4b9871a796e114
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-26 17:32:22 +0500

    Add service to invoke solver's methods

---
+++ KnapsackProblem.Blazor/Data/ImplementationInvokeService.cs
@@ -0,0 +1,30 @@
+﻿using KnapsackProblem.BlazorApp.Data.Implementations;
+using System.Linq;
+
+namespace KnapsackProblem.BlazorApp.Data
+{
+    public class ImplementationInvokeService
+    {
+        private readonly InputService _inputService;
+
+        public ImplementationInvokeService(InputService inputService)
+        {
+            _inputService = inputService;
+        }
+
+        public bool[] InvokeSelected(ISolver implementation)
+        {
+            var input = _inputService.Input;
+
+            var maxWeight = input.Knapsack.MaxWeight;
+            var weights = input.Items
+                .Select(it => it.Weight)
+                .ToArray();
+            var costs = input.Items
+                .Select(it => it.Cost)
+                .ToArray();
+
+            return implementation.Solve(maxWeight, weights, costs);
+        }
+    }
+}

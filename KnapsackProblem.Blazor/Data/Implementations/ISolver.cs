04fd664f11693704e13c8db3c797e7a3abef67d1
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-26 18:26:13 +0500

    Replace interface file with DLL

--- KnapsackProblem.Blazor/Data/Implementations/ISolver.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KnapsackProblem.BlazorApp.Data.Implementations
-{
-    public interface ISolver
-    {
-        bool[] Solve(int M, int[] m, int[] c);
-        string GetName();
-    }
-}

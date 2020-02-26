e90a4f35c7c5a9bffbe5f5ff0b7adaa362241b5f
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-26 18:25:56 +0500

    Add F# backend project

---
+++ KnapsackProblem.Implementation.FSharp/Library.fs
@@ -0,0 +1,13 @@
+﻿namespace KnapsackProblem.Implementation.FSharp
+
+open Knapsack
+
+type public MySuperSolver() =
+  interface ISolver with
+    member _this.GetName() = "Самый лучший алгоритм"
+    member _this.Solve(M, m, c) = 
+      let weights = List.ofArray m // Теперь в weights обычный F#-список масс ...
+      let costs = List.ofArray c   // ... а в costs  - обычный F#-список стоимостей
+      let solve M (w1::wTail) (c1::cTail) =
+        if w1 < M then [true] else [false] // вместо этой "заглушки" должна быть собственно реализация алгоритма
+      List.toArray (solve M weights costs) // в конце с помощью toArray F#-список превращается в массив

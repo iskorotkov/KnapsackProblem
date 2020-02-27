bf843339de7eaf4a988295e287264b37b0c84661
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-02-27 15:42:02 +0500

    Implement F# backend

--- KnapsackProblem.Implementation.FSharp/Library.fs
+++ KnapsackProblem.Implementation.FSharp/Library.fs
@@ -1,13 +1,22 @@
 ﻿namespace KnapsackProblem.Implementation.FSharp
 
 open Knapsack
+open System
 
 type public MySuperSolver() =
   interface ISolver with
     member _this.GetName() = "Самый лучший алгоритм"
-    member _this.Solve(M, m, c) = 
-      let weights = List.ofArray m // Теперь в weights обычный F#-список масс ...
-      let costs = List.ofArray c   // ... а в costs  - обычный F#-список стоимостей
-      let solve M (w1::wTail) (c1::cTail) =
-        if w1 < M then [true] else [false] // вместо этой "заглушки" должна быть собственно реализация алгоритма
-      List.toArray (solve M weights costs) // в конце с помощью toArray F#-список превращается в массив
+    member _this.Solve(M, m, c) =
+      let weights = List.ofArray m
+      let costs = List.ofArray c
+      let rec solve max (weights, costs) (curWeight, curCost, curFlags) =
+        if curWeight > max then (0, [])
+        else match (weights, costs) with
+              | ([], []) -> (curCost, curFlags)
+              | (wh::wt, ch::ct) ->
+                  let res1 = solve max (wt, ct) (curWeight, curCost, false::curFlags)
+                  let res2 = solve max (wt, ct) (curWeight+wh, curCost+ch, true::curFlags)
+                  if fst res1 > fst res2 then res1 else res2
+              | _ -> (0, [])
+      let represent max weights costs = List.rev (snd (solve max (weights, costs) (0, 0, [])))
+      List.toArray (represent M weights costs)
